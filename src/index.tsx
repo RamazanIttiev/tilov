cdff6426dc13d4ba203d41d810ffde555cf5aad6
Ramazan Ittiev <rittiev@devexperts.com>
2022-01-26 16:42:26 +0300

    [3] index.tsx file set up and theme file configured

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
 
 const App = () => {
-    console.log('dsd');
-    return <div>Hello</div>;
+    return (
+        <React.StrictMode>
+            <ThemeProvider theme={theme}>
+                <StyledThemeProvider theme={theme}>
+                    <CssBaseline />
+                </StyledThemeProvider>
+            </ThemeProvider>
+        </React.StrictMode>
+    );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
