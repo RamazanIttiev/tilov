93ccd0d411c911800a0787bc39da3ff90899b8bc
Ramazan <ramazzan.itteiv@gmail.com>
2022-02-03 18:04:15 +0300

    [19] Footer refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,40 +1,49 @@
 import React from 'react';
-import { Box, IconButton, Link } from '@mui/material';
+import { Box, Grid, IconButton, Link } from '@mui/material';
 import { Instagram, Telegram, WhatsApp } from '@mui/icons-material';
+import { Form } from './Form';
 
 export const Footer = () => {
   return (
-    <Box
-      component="footer"
-      sx={{
-        minHeight: '72px',
-        p: 2,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-      }}
-    >
-      <IconButton>
-        <Instagram />
-      </IconButton>
-      <IconButton>
-        <WhatsApp />
-      </IconButton>
-      <IconButton>
-        <Telegram />
-      </IconButton>
-      <Link
+    <Grid container spacing={4} component="footer" sx={{ mt: 12 }}>
+      <Grid
+        item
+        md={4}
         sx={{
-          width: '100%',
-          textAlign: 'center',
-          textDecoration: 'none',
-          color: 'inherit',
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor: theme => theme.palette.secondary.main,
+          color: theme => theme.palette.secondary.contrastText,
         }}
-        href="tel:+79215588401"
       >
-        +7 (921) 558-84-01
-      </Link>
-    </Box>
+        <Box>
+          <IconButton sx={{ color: '#000', height: '42px' }}>
+            <Instagram />
+          </IconButton>
+          <IconButton sx={{ color: '#000', height: '42px' }}>
+            <WhatsApp />
+          </IconButton>
+          <IconButton sx={{ color: '#000', height: '42px' }}>
+            <Telegram />
+          </IconButton>
+        </Box>
+        <Link
+          sx={{
+            width: '100%',
+            textAlign: 'center',
+            textDecoration: 'none',
+            color: 'inherit',
+          }}
+          href="tel:+79215588401"
+        >
+          +7 (921) 558-84-01
+        </Link>
+      </Grid>
+      <Grid item md={8}>
+        <Form />
+      </Grid>
+    </Grid>
   );
 };
